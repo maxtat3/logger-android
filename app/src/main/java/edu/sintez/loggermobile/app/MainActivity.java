74a0bf450394db97f563c30d6e5014d984d40a1c
Max <multi3volume@gmail.com>
2016-06-08 18:32:20 +0300

    One point and dataset added.

--- app/src/main/java/edu/sintez/loggermobile/app/MainActivity.java
+++ app/src/main/java/edu/sintez/loggermobile/app/MainActivity.java
@@ -3,15 +3,21 @@ package edu.sintez.loggermobile.app;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity implements OnChartValueSelectedListener {
 
@@ -68,4 +74,72 @@ public class MainActivity extends Activity implements OnChartValueSelectedListen
 	public void onNothingSelected() {
 
 	}
+
+	private void addEntry(float val1) {
+		LineData data = lineChart.getData();
+
+		if(data != null) {
+			ILineDataSet set = data.getDataSetByIndex(0);
+
+			if (set == null) {
+				set = createSet();
+				data.addDataSet(set);
+			}
+
+			// add a new x-value first
+			data.addXValue(set.getEntryCount() + "");
+			data.addEntry(new Entry(val1, set.getEntryCount()), 0);
+
+			lineChart.notifyDataSetChanged();
+
+			lineChart.setVisibleXRangeMaximum(300);
+			lineChart.setVisibleYRangeMaximum(200, YAxis.AxisDependency.LEFT);
+//          // this automatically refreshes the chart (calls invalidate())
+			lineChart.moveViewTo(data.getXValCount()-7, 50f, YAxis.AxisDependency.LEFT);
+		}
+	}
+
+	private void addDataSet() {
+		LineData data = lineChart.getData();
+
+		if(data != null) {
+			Log.d(LOG, "data.getDataSetCount() = " + data.getDataSetCount());
+			int count = (data.getDataSetCount() + 1);
+			Log.d(LOG, "data.getDataSetCount() = " + data.getDataSetCount());
+
+			// create 10 y-vals
+			ArrayList<Entry> yVals = new ArrayList<Entry>();
+
+			Log.d(LOG, "data.getXValCount() = " + (data.getXValCount()));
+
+			LineDataSet set = new LineDataSet(yVals, "DataSet " + count);
+			set.setLineWidth(2.5f);
+			set.setCircleRadius(0);
+
+			int color = colors[count % colors.length];
+
+			set.setColor(color);
+			set.setCircleColor(color);
+			set.setHighLightColor(color);
+			set.setValueTextSize(10f);
+			set.setValueTextColor(color);
+
+			data.addDataSet(set);
+			lineChart.notifyDataSetChanged();
+			lineChart.invalidate();
+		}
+	}
+
+	private LineDataSet createSet() {
+		LineDataSet set = new LineDataSet(null, "DataSet 1");
+		set.setLineWidth(2.5f);
+		set.setCircleRadius(0);
+		set.setColor(Color.rgb(240, 99, 99));
+		set.setCircleColor(Color.rgb(240, 99, 99));
+		set.setHighLightColor(Color.rgb(190, 190, 190));
+		set.setAxisDependency(YAxis.AxisDependency.LEFT);
+		set.setValueTextSize(10f);
+		return set;
+	}
+
 }
