811b0eed3a39f86e1fcd17329fa5b779baddbd76
Max <multi3volume@gmail.com>
2016-06-08 19:37:37 +0300

    Add "handler" for menu item for start and stop measure process from BT. This send <b> command to mcu device for start measure process. Mcu device must be reposed data.

--- app/src/main/java/edu/sintez/loggermobile/app/MainActivity.java
+++ app/src/main/java/edu/sintez/loggermobile/app/MainActivity.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.Toast;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
@@ -107,17 +108,11 @@ public class MainActivity extends Activity implements OnChartValueSelectedListen
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
+		if(item.getItemId() == R.id.mi_action_call_mcu) {
+			connectedThread.write("b");
+			Toast.makeText(this, "Entry added!", Toast.LENGTH_SHORT).show();
 		}
-
-		return super.onOptionsItemSelected(item);
+		return true;
 	}
 
 	@Override
